5c5c8b8123f37d920adec49897ef0e966a0677f1
John Gambrell <john.gambrell@gmail.com>
2024-05-31 11:04:53 -0500

    defined CartAPIManager

---
+++ GMarketplace/Networking/CartAPIManager.swift
@@ -0,0 +1,13 @@
+
+
+import Foundation
+enum CartURL: String {
+   // case getCatalog = "http://192.168.0.126:3000/catalog"
+    case getCart = "http://localhost:3000/cart"
+}
+
+
+struct CartAPIManager {
+//    func fetchCart(from url: CatalogURL) async -> [CatalogModel] {
+//    }
+}
